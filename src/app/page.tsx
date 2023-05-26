45ade139c5122a1bc1fc6f62d5759cd4111d4684
Troy Feng <troy.feng@yale.edu>
2023-05-26 18:55:35 -0400

    linting ignore + run pretty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,20 @@ export default function Home() {
                     <code className={styles.code}>src/app/page.tsx</code>
                 </p>
                 <div>
-                    <a href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
-                        By <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        By{' '}
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.vercelLogo}
+                            width={100}
+                            height={24}
+                            priority
+                        />
                     </a>
                 </div>
             </div>
@@ -23,14 +35,24 @@ export default function Home() {
             </div>
 
             <div className={styles.grid}>
-                <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
+                <a
+                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <h2>
                         Docs <span>-&gt;</span>
                     </h2>
                     <p>Find in-depth information about Next.js features and API.</p>
                 </a>
 
-                <a href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
+                <a
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <h2>
                         Learn <span>-&gt;</span>
                     </h2>
@@ -49,7 +71,12 @@ export default function Home() {
                     <p>Explore the Next.js 13 playground.</p>
                 </a>
 
-                <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
+                <a
+                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <h2>
                         Deploy <span>-&gt;</span>
                     </h2>
