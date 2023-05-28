5dcc178661cb27c3b96014672d0c33ac5d5d79ca
Troy Feng <troy.feng@yale.edu>
2023-05-27 21:15:51 -0400

    Clean up server lib (GH access token APIs)

--- src/app/api/get-token/route.ts
+++ src/app/api/get-token/route.ts
@@ -1,6 +1,5 @@
 import { GetTokenAPIResponse } from '@/models/api'
-import { GH_OAuthAccessTokenAPIError, GH_OAuthAccessTokenAPISuccess } from '@/models/gh'
-import { GH_CLIENT_ID, GH_CLIENT_SECRET } from '@/server/constants'
+import { GH_getTokenWithClientCodeAPI } from '@/server/lib/gh-auth'
 
 export const POST = async (request: Request): Promise<Response> => {
     console.log('/api/get-token POST')
@@ -19,49 +18,16 @@ export const POST = async (request: Request): Promise<Response> => {
         })
     }
 
-    const payload = {
-        code: code,
-        client_id: GH_CLIENT_ID,
-        client_secret: GH_CLIENT_SECRET,
-    }
-    console.log(payload)
-
-    const res = await fetch('https://github.com/login/oauth/access_token', {
-        method: 'POST',
-        body: JSON.stringify(payload),
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-        },
-    })
-
-    const resJson = await res.json()
-    console.log(resJson)
-
-    const { error: unknownError, access_token: unknownAccessToken } = resJson
-    if (unknownError !== undefined || unknownAccessToken === undefined) {
-        // type cast
-        const ghResJson = resJson as GH_OAuthAccessTokenAPIError
-        const { error, error_description } = ghResJson
-        const clientRes: GetTokenAPIResponse = {
-            success: false,
-            error: `${error}: ${error_description}`,
-        }
-        return new Response(JSON.stringify(clientRes), {
+    const res = await GH_getTokenWithClientCodeAPI(code)
+    const { success, error, accessToken, refreshToken } = res
+    if (!success || accessToken === undefined || refreshToken === undefined) {
+        return new Response(JSON.stringify(res), {
             status: 400,
+            statusText: error,
         })
     }
 
-    const ghResJson = resJson as GH_OAuthAccessTokenAPISuccess
-    const { access_token, refresh_token } = ghResJson
-
-    const clientRes: GetTokenAPIResponse = {
-        success: true,
-        accessToken: access_token,
-        refreshToken: refresh_token,
-    }
-
-    return new Response(JSON.stringify(clientRes), {
+    return new Response(JSON.stringify(res), {
         status: 200,
     })
 }
--- src/server/lib/gh-auth/index.ts
+++ src/server/lib/gh-auth/index.ts
@@ -2,6 +2,52 @@ import { GetTokenAPIResponse } from '@/models/api'
 import { GH_OAuthAccessTokenAPIError, GH_OAuthAccessTokenAPISuccess } from '@/models/gh'
 import { GH_CLIENT_ID, GH_CLIENT_SECRET } from '@/server/constants'
 
+const processTokenRes = (resJson: unknown): GetTokenAPIResponse => {
+    const { error: unknownError, access_token: unknownAccessToken } = resJson as Partial<GH_OAuthAccessTokenAPIError> &
+        Partial<GH_OAuthAccessTokenAPISuccess>
+    if (unknownError !== undefined || unknownAccessToken === undefined) {
+        // type cast
+        const ghResJson = resJson as GH_OAuthAccessTokenAPIError
+        const { error, error_description } = ghResJson
+        return {
+            success: false,
+            error: `${error}: ${error_description}`,
+        }
+    }
+
+    const ghResJson = resJson as GH_OAuthAccessTokenAPISuccess
+    const { access_token, refresh_token } = ghResJson
+    return {
+        success: true,
+        accessToken: access_token,
+        refreshToken: refresh_token,
+    }
+}
+
+export const GH_getTokenWithClientCodeAPI = async (code: string): Promise<GetTokenAPIResponse> => {
+    const payload = {
+        code: code,
+        client_id: GH_CLIENT_ID,
+        client_secret: GH_CLIENT_SECRET,
+    }
+    console.log(payload)
+
+    const res = await fetch('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    })
+
+    const resJson = await res.json()
+    console.log(resJson)
+
+    return processTokenRes(resJson)
+}
+
+// https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/refreshing-user-access-tokens
 export const GH_refreshTokenAPI = async (refreshToken: string): Promise<GetTokenAPIResponse> => {
     const payload = {
         refresh_token: refreshToken,
@@ -23,23 +69,5 @@ export const GH_refreshTokenAPI = async (refreshToken: string): Promise<GetToken
     const resJson = await res.json()
     console.log(resJson)
 
-    const { error: unknownError, access_token: unknownAccessToken } = resJson
-    if (unknownError !== undefined || unknownAccessToken === undefined) {
-        // type cast
-        const ghResJson = resJson as GH_OAuthAccessTokenAPIError
-        const { error, error_description } = ghResJson
-        return {
-            success: false,
-            error: `${error}: ${error_description}`,
-        }
-    }
-
-    const ghResJson = resJson as GH_OAuthAccessTokenAPISuccess
-    const { access_token: updatedAccessToken, refresh_token: updatedRefreshToken } = ghResJson
-
-    return {
-        success: true,
-        accessToken: updatedAccessToken,
-        refreshToken: updatedRefreshToken,
-    }
+    return processTokenRes(resJson)
 }
