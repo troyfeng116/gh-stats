691e608db70f73e70de0f9df9b38665fd79fe2ed
Troy Feng <troy.feng@yale.edu>
2023-05-27 22:59:24 -0400

    Login routing

--- src/components/client/Login/Login.tsx
+++ src/components/client/Login/Login.tsx
@@ -1,11 +1,25 @@
 'use client'
 
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 import { NEXT_PUBLIC_GH_CLIENT_ID } from '@/client/constants'
+import { AuthStatus, useAuth } from '@/components/client/Auth'
 
 export const Login: React.FC = () => {
+    const router = useRouter()
+    const { authStatus } = useAuth()
+
     const loginURL = `https://github.com/login/oauth/authorize?client_id=${NEXT_PUBLIC_GH_CLIENT_ID}`
+
+    if (authStatus === AuthStatus.INITIALIZING) {
+        return <div></div>
+    }
+
+    if (authStatus === AuthStatus.AUTH) {
+        router.push('/')
+        return <div></div>
+    }
 
     return (
         <div>
