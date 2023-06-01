6900b2580b1cae1c193545b5564ea22814843e8c
Troy Feng <troyfeng1@gmail.com>
2023-06-01 14:41:17 -0400

    Refactor userCard service

--- src/app/api/user-card/route.ts
+++ src/app/api/user-card/route.ts
@@ -1,5 +1,4 @@
-// import { getUserCardData } from '@/server/services/userCardService'
-import { getUserCardDataFromGQL } from '@/server/services/gql/userCardService'
+import { getUserCardDataFromGQL } from '@/server/services/userCard'
 import { AUTH_NO_TOKEN_ERROR_RES, checkAuthHeaders } from '@/server/utils/authHeaders'
 import { SHARED_APIFields__GetUserCard } from '@/shared/models'
 
@@ -18,22 +17,7 @@ export const GET = async (request: Request): Promise<Response> => {
         })
     }
 
-    // const userCardRes: SHARED_GetUserCardAPIResponse = await getUserCardData(token)
     const userCardRes: SHARED_APIFields__GetUserCard = await getUserCardDataFromGQL(token)
-    // const userCardRes: SHARED_GetUserCardAPIResponse = {
-    //     success: true,
-    //     userCard: {
-    //         userId: 'troyfeng999',
-    //         name: 'Troy Feng',
-    //         email: 'troy9@gmail.com',
-    //         followers: 5,
-    //         following: 5,
-    //         createdAt: '4/8/2020, 11:07:35 PM',
-    //         publicRepos: 20,
-    //         privateRepos: 9,
-    //         totalRepos: 29,
-    //     },
-    // }
     const { success, userCard } = userCardRes
 
     const status = !success || userCard === undefined ? 400 : 200
--- src/server/services/userCard/index.ts
+++ src/server/services/userCard/index.ts
@@ -1,3 +1,4 @@
+import { GH_API_Call__getUser } from '@/server/lib/gh-api/users'
 import { GH_GQL_Call__Viewer } from '@/server/lib/gh-gql/Viewer'
 import { SHARED_APIFields__GetUserCard } from '@/shared/models'
 
@@ -30,6 +31,31 @@ export const getUserCardDataFromGQL = async (accessToken: string): Promise<SHARE
             publicRepos: 0,
             privateRepos: 0,
             totalRepos: totalRepos,
+        },
+    }
+}
+
+export const getUserCardDataFromAPI = async (accessToken: string): Promise<SHARED_APIFields__GetUserCard> => {
+    const { user, success, error } = await GH_API_Call__getUser(accessToken)
+
+    if (!success || user === undefined) {
+        return { success: false, error: error, userCard: undefined }
+    }
+
+    const { login, name, email, followers, following, created_at, public_repos, total_private_repos } = user
+
+    return {
+        success: true,
+        userCard: {
+            userId: login,
+            name: name === null ? undefined : name,
+            email: email == null ? undefined : email,
+            followers: followers,
+            following: following,
+            createdAt: created_at,
+            publicRepos: public_repos,
+            privateRepos: total_private_repos,
+            totalRepos: public_repos + total_private_repos,
         },
     }
 }
