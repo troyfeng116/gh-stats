43a68721a4df0447adabf6875f424ca1e176b942
Troy Feng <troyfeng1@gmail.com>
2023-06-18 18:34:57 -0500

    Refactor Repos + LifetimeStats component (data fetching + logic in Repos)

--- src/client/components/Pages/Auth/Repos/LifetimeStats/LifetimeStats.tsx
+++ src/client/components/Pages/Auth/Repos/LifetimeStats/LifetimeStats.tsx
@@ -1,50 +1,19 @@
 import styles from './LifetimeStats.module.css'
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import LanguageData from './LanguageData'
 import RepoCell from './RepoCell'
 
-import { lifetimeStatsAPI } from '@/client/lib/authAPI'
 import { SHARED_Model__LifetimeStats } from '@/shared/models/apiFields/lifetimeStats'
 import { kbToStr } from '@/shared/utils/toBytesStr'
 
 interface LifetimeStatsProps {
-    accessToken: string
+    lifetimeStats: SHARED_Model__LifetimeStats
 }
 
 export const LifetimeStats: React.FC<LifetimeStatsProps> = (props) => {
-    const { accessToken } = props
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const [error, setError] = useState<string>()
-    const [lifetimeStats, setLifetimeStats] = useState<SHARED_Model__LifetimeStats>()
-
-    useEffect(() => {
-        const fetchUserCard = async (accessToken: string) => {
-            setError(undefined)
-            const { success, error, lifetimeStats } = await lifetimeStatsAPI(accessToken)
-            setIsLoading(false)
-            if (!success || lifetimeStats === undefined) {
-                setError(error)
-            } else {
-                setLifetimeStats(lifetimeStats)
-            }
-        }
-
-        fetchUserCard(accessToken)
-    }, [accessToken])
-
-    if (isLoading) {
-        return <div>lifetime stats loading...</div>
-    }
-
-    if (error !== undefined) {
-        return <div>{error}</div>
-    }
-
-    if (lifetimeStats === undefined) {
-        return <div>no lifetime stats found</div>
-    }
+    const { lifetimeStats } = props
 
     const { repos, rc_stats, language_stats } = lifetimeStats
     const { numRepos, numCommits } = rc_stats
--- src/client/components/Pages/Auth/Repos/Repos.tsx
+++ src/client/components/Pages/Auth/Repos/Repos.tsx
@@ -1,26 +1,48 @@
 'use client'
 
-import React from 'react'
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
 
 import LifetimeStats from './LifetimeStats'
 
 import { useAuth } from '@/client/components/Wrappers/AuthProvider'
+import { lifetimeStatsAPI } from '@/client/lib/authAPI'
+import { SHARED_Model__LifetimeStats } from '@/shared/models/apiFields/lifetimeStats'
 
 export const Repos: React.FC = () => {
     const { accessToken } = useAuth()
 
-    if (accessToken === undefined) {
-        return (
-            <div>
-                <Link href="/login">Please login</Link>
-            </div>
-        )
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string>()
+    const [lifetimeStats, setLifetimeStats] = useState<SHARED_Model__LifetimeStats>()
+
+    useEffect(() => {
+        const fetchUserCard = async (accessToken: string) => {
+            setError(undefined)
+            const { success, error, lifetimeStats } = await lifetimeStatsAPI(accessToken)
+            setIsLoading(false)
+            if (!success || lifetimeStats === undefined) {
+                setError(error)
+            } else {
+                setLifetimeStats(lifetimeStats)
+            }
+        }
+
+        if (accessToken !== undefined) {
+            fetchUserCard(accessToken)
+        }
+    }, [accessToken])
+
+    if (isLoading) {
+        return <div>lifetime stats loading...</div>
+    }
+
+    if (error !== undefined || lifetimeStats === undefined) {
+        return <div>{error}</div>
     }
 
     return (
         <div>
-            <LifetimeStats accessToken={accessToken} />
+            <LifetimeStats lifetimeStats={lifetimeStats} />
         </div>
     )
 }
