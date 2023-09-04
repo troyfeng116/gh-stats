0485baae6209ab6a43e20d47edacaae2cafa3b6c
Troy Feng <troyfeng1@gmail.com>
2023-09-04 18:15:19 -0400

    Reusable Logo component, refactor Nav to include logo in all auth states

--- src/client/components/Wrappers/Nav/Nav.tsx
+++ src/client/components/Wrappers/Nav/Nav.tsx
@@ -1,48 +1,51 @@
 import styles from './Nav.module.css'
 
 import React from 'react'
-import { FaGithub } from 'react-icons/fa'
 import Link from 'next/link'
 
 import Button from '@/client/components/Reuse/Button'
+import { Logo } from '@/client/components/Reuse/Logo/Logo'
 import { AuthStatus, useAuth } from '@/client/components/Wrappers/AuthProvider'
 import { NAV_AUTH_ROUTES, NAV_UNAUTH_ROUTES } from '@/client/routes'
 
 export const Nav: React.FC = () => {
     const { authStatus, logout } = useAuth()
 
-    if (authStatus === AuthStatus.INITIALIZING) {
-        return <nav className={styles.nav}></nav>
-    }
-
-    if (authStatus === AuthStatus.AUTH) {
-        return (
-            <nav className={styles.nav}>
-                <div className={styles.nav_left}>
-                    <Link href="/" className={`${styles.nav_link} ${styles.nav_link_logo}`}>
-                        <FaGithub size={27} />
-                        stats
-                    </Link>
-                    {NAV_AUTH_ROUTES.map(({ href, label }, idx) => (
-                        <Link key={idx} href={href} className={styles.nav_link}>
-                            {label}
-                        </Link>
-                    ))}
-                </div>
-                <Button onClick={logout}>Log out</Button>
-            </nav>
-        )
-    }
-
-    return (
-        <nav className={styles.nav}>
-            <div>
+    let navLeft: React.ReactElement | null = null
+    let navRight: React.ReactElement | null = null
+    if (authStatus === AuthStatus.UNAUTH) {
+        navLeft = (
+            <>
                 {NAV_UNAUTH_ROUTES.map(({ href, label }, idx) => (
                     <Link key={idx} href={href} className={styles.nav_link}>
                         {label}
                     </Link>
                 ))}
+            </>
+        )
+    } else if (authStatus === AuthStatus.AUTH) {
+        navLeft = (
+            <>
+                {NAV_AUTH_ROUTES.map(({ href, label }, idx) => (
+                    <Link key={idx} href={href} className={styles.nav_link}>
+                        {label}
+                    </Link>
+                ))}
+            </>
+        )
+
+        navRight = <Button onClick={logout}>Log out</Button>
+    }
+
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.nav_left}>
+                <Link href="/" className={`${styles.nav_link} ${styles.nav_link_logo}`}>
+                    <Logo />
+                </Link>
+                {navLeft}
             </div>
+            {navRight}
         </nav>
     )
 }
