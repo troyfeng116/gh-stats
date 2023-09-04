5028120f1145a3a24d21fa6fa1fc7eced4bdf26a
Troy Feng <troyfeng1@gmail.com>
2023-09-03 22:18:32 -0400

    Refactor lifetime stats SHRED_Model into models

--- src/shared/models/apiFields/lifetimeStats.ts
+++ src/shared/models/apiFields/lifetimeStats.ts
@@ -1,14 +1,6 @@
-import { SHARED_Model__AllLanguageStats } from '../models/Language'
-import { SHARED_Model__RepoWithCommitCountsAndLanguages } from '../models/Repos'
-import { SHARED_Model__RepoCommitCountStats } from '../models/Stats'
+import { SHARED_Model__LifetimeStats } from '../models/Stats'
 
 import { SHARED_APIFields__BASE } from '.'
-
-export interface SHARED_Model__LifetimeStats {
-    language_stats: SHARED_Model__AllLanguageStats
-    repos: SHARED_Model__RepoWithCommitCountsAndLanguages[]
-    rc_stats: SHARED_Model__RepoCommitCountStats
-}
 
 export interface SHARED_APIFields__LifetimeStats extends SHARED_APIFields__BASE {
     lifetimeStats?: SHARED_Model__LifetimeStats
