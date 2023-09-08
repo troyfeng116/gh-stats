d8c4b5b8a8ab694c8f5de5b50aaa70bdfe415593
Troy Feng <troyfeng1@gmail.com>
2023-09-08 17:14:11 -0400

    Add quartile adjustments to calender grid on server

--- src/shared/models/converters/ContributionsConverters.ts
+++ src/shared/models/converters/ContributionsConverters.ts
@@ -1,11 +1,14 @@
 import {
     SHARED_Model__CommitContributionsByRepo,
+    SHARED_Model__ContributionCalendarWeek,
+    SHARED_Model__ContributionLevelType,
     SHARED_Model__ContributionsAggregate,
     SHARED_Model__ContributionsCollection,
 } from '../models/Contributions'
 
 import { GH_GQL_Schema__ContributionsCollection } from '@/server/lib/gh-gql/Contributions/query'
 import { GH_GQL_Schema__ContributionsAggregate } from '@/server/lib/gh-gql/ContributionsAggregate/query'
+import { computeQuartiles, getQuartileForPoint } from '@/shared/utils/computeQuartiles'
 import { getRepoKey } from '@/shared/utils/getRepoKeyFromRepoContributions'
 
 export const CONVERTER__contributionsAggregateSchemaToShared = (
@@ -59,6 +62,48 @@ export const REDUCER__contributionsAggregateMerger = (
     }
 }
 
+const QUARTILE_TO_COLOR: { [quartile in SHARED_Model__ContributionLevelType]: string } = {
+    [SHARED_Model__ContributionLevelType.NONE]: '#ebedf0',
+    [SHARED_Model__ContributionLevelType.FIRST_QUARTILE]: '#9be9a8',
+    [SHARED_Model__ContributionLevelType.SECOND_QUARTILE]: '#40c463',
+    [SHARED_Model__ContributionLevelType.THIRD_QUARTILE]: '#30a14e',
+    [SHARED_Model__ContributionLevelType.FOURTH_QUARTILE]: '#216e39',
+}
+
+/**
+ * When merging weeks, recompute quartile values for each day.
+ */
+const REDUCER__contributionWeeksMerger = (
+    w1: SHARED_Model__ContributionCalendarWeek[],
+    w2: SHARED_Model__ContributionCalendarWeek[],
+): SHARED_Model__ContributionCalendarWeek[] => {
+    const mergedWeeks: SHARED_Model__ContributionCalendarWeek[] = [...w1, ...w2]
+    const flattenedContributionCounts: number[] = mergedWeeks
+        .map(({ contributionDays }) => contributionDays)
+        .flat(1)
+        .map(({ contributionCount }) => contributionCount)
+
+    const updatedQuartiles = computeQuartiles(flattenedContributionCounts.filter((ct) => ct > 0))
+    for (const week of mergedWeeks) {
+        const { contributionDays } = week
+        for (const day of contributionDays) {
+            const { contributionCount } = day
+            if (contributionCount === 0) {
+                continue
+            }
+
+            const updatedQuartile = getQuartileForPoint(contributionCount, updatedQuartiles)
+            day.contributionLevel = updatedQuartile
+            day.color = QUARTILE_TO_COLOR[updatedQuartile]
+        }
+    }
+
+    return mergedWeeks
+}
+
+/**
+ * When merging contributions by repo, avoid duplicating repository contributions and aggregate by repository name.
+ */
 const REDUCER__contributionsByRepositoryMerger = (
     c1: SHARED_Model__CommitContributionsByRepo[],
     c2: SHARED_Model__CommitContributionsByRepo[],
@@ -89,6 +134,9 @@ export const REDUCER__contributionsCollection_INITIAL_VALUE: SHARED_Model__Contr
     commitContributionsByRepository: [],
 }
 
+/**
+ * Merge two contributions collections, for use when re-aggregating after breaking up a time range into chunks.
+ */
 export const REDUCER__contributionsCollectionMerger = (
     c1: SHARED_Model__ContributionsCollection,
     c2: SHARED_Model__ContributionsCollection,
@@ -97,9 +145,8 @@ export const REDUCER__contributionsCollectionMerger = (
         ...REDUCER__contributionsAggregateMerger(c1, c2),
         contributionCalendar: {
             isHalloween: c1.contributionCalendar.isHalloween && c2.contributionCalendar.isHalloween,
-            weeks: [...c1.contributionCalendar.weeks, ...c2.contributionCalendar.weeks],
+            weeks: REDUCER__contributionWeeksMerger(c1.contributionCalendar.weeks, c2.contributionCalendar.weeks),
         },
-        // commitContributionsByRepository: [...c1.commitContributionsByRepository, ...c2.commitContributionsByRepository],
         commitContributionsByRepository: REDUCER__contributionsByRepositoryMerger(
             c1.commitContributionsByRepository,
             c2.commitContributionsByRepository,
