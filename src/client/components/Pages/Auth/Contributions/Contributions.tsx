1535ad10f68c4b9179e68b766b09f6fdbabcdcd4
Troy Feng <troyfeng1@gmail.com>
2023-09-12 22:20:27 -0400

    Refactor Contributions state management into reducer

--- src/client/components/Pages/Auth/Contributions/Contributions.tsx
+++ src/client/components/Pages/Auth/Contributions/Contributions.tsx
@@ -1,44 +1,56 @@
 'use client'
 
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useReducer } from 'react'
 
+import {
+    CONTRIBUTIONS_FETCH_ERROR,
+    CONTRIBUTIONS_FETCH_SUCCESS,
+    CONTRIBUTIONS_LOADING,
+    CONTRIBUTIONS_TRIGGER_CUSTOM_RANGE_QUERY,
+} from './actions'
 import ContributionsMain from './ContributionsMain'
+import { reducer } from './reducer'
 
 import DateRangeInput from '@/client/components/Reuse/DateRangeInput'
 import { useAuth } from '@/client/components/Wrappers/AuthProvider'
 import { contributionsAPI } from '@/client/lib/authAPI'
 import { SHARED_Model__ContributionsClientInfo } from '@/shared/models/models/Contributions'
 
+export interface ContributionsState {
+    isLoading: boolean
+    contributionsFetchError?: string
+    contributionsClientInfo?: SHARED_Model__ContributionsClientInfo
+    queryDateRange: { from?: string; to?: string }
+    shouldTriggerRangeFetch: boolean
+}
+
+const initialContributionsState: ContributionsState = {
+    isLoading: true,
+    contributionsFetchError: undefined,
+    contributionsClientInfo: undefined,
+    queryDateRange: { from: undefined, to: undefined },
+    shouldTriggerRangeFetch: false,
+}
+
 export const Contributions: React.FC = () => {
     const { accessToken } = useAuth()
 
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const [error, setError] = useState<string>()
-    const [contributionsClientInfo, setContributionsClientInfo] = useState<SHARED_Model__ContributionsClientInfo>()
-
-    const [triggerFetch, setTriggerFetch] = useState<boolean>(false)
-    const [queryDateRange, setQueryDateRange] = useState<{ from?: string; to?: string }>({
-        from: undefined,
-        to: undefined,
-    })
+    const [state, dispatch] = useReducer(reducer, initialContributionsState)
+    const { isLoading, contributionsFetchError, contributionsClientInfo, queryDateRange, shouldTriggerRangeFetch } =
+        state
 
     const fetchContributions = useCallback(async (accessToken: string, from?: string, to?: string) => {
-        // TODO: reducer
-        setError(undefined)
-        setTriggerFetch(false)
-
-        setIsLoading(true)
+        dispatch({ type: CONTRIBUTIONS_LOADING })
         const {
             success,
             error,
             contributionsClientInfo: updatedContributionsClientInfo,
         } = await contributionsAPI(accessToken, from, to)
-        setIsLoading(false)
 
         if (!success || updatedContributionsClientInfo === undefined) {
-            setError(error)
+            dispatch({ type: CONTRIBUTIONS_FETCH_ERROR, contributionsFetchError: error })
         } else {
-            setContributionsClientInfo(updatedContributionsClientInfo)
+            dispatch({ type: CONTRIBUTIONS_FETCH_SUCCESS, contributionsClientInfo: updatedContributionsClientInfo })
         }
     }, [])
 
@@ -51,25 +63,22 @@ export const Contributions: React.FC = () => {
 
     // custom lookback
     useEffect(() => {
-        setTriggerFetch(false)
-
-        if (triggerFetch && accessToken !== undefined) {
+        if (shouldTriggerRangeFetch && accessToken !== undefined) {
             const { from: curFrom, to: curTo } = queryDateRange
             // TODO: client-side validate dates
             fetchContributions(accessToken, curFrom, curTo)
         }
-    }, [accessToken, triggerFetch, queryDateRange])
+    }, [accessToken, shouldTriggerRangeFetch, queryDateRange])
 
     const handleRangeSelected = (fromDate: string | undefined, toDate: string | undefined) => {
-        setQueryDateRange({ from: fromDate, to: toDate })
-        setTriggerFetch(true)
+        dispatch({ type: CONTRIBUTIONS_TRIGGER_CUSTOM_RANGE_QUERY, queryDateRange: { from: fromDate, to: toDate } })
     }
 
     let contributionsMainComponent: React.ReactNode | null = null
     if (isLoading) {
         contributionsMainComponent = <div>Loading</div>
-    } else if (error !== undefined || contributionsClientInfo === undefined) {
-        contributionsMainComponent = <div>{error}</div>
+    } else if (contributionsFetchError !== undefined || contributionsClientInfo === undefined) {
+        contributionsMainComponent = <div>{contributionsFetchError}</div>
     } else {
         contributionsMainComponent = <ContributionsMain contributionsClientInfo={contributionsClientInfo} />
     }
