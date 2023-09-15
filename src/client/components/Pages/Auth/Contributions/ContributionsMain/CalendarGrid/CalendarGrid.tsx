562b2e000c20bc866ece5edccf99ea61b13ddf1f
Troy Feng <troyfeng1@gmail.com>
2023-09-15 19:45:56 -0400

    Reusable OverflowScroll component; integrate into CalendarGrid

--- src/client/components/Pages/Auth/Contributions/ContributionsMain/CalendarGrid/CalendarGrid.tsx
+++ src/client/components/Pages/Auth/Contributions/ContributionsMain/CalendarGrid/CalendarGrid.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 import Row from './Row'
 
+import OverflowScroll from '@/client/components/Reuse/OverflowScroll'
 import { SHARED_Model__ContributionCalendarDay } from '@/shared/models/models/Contributions'
 import { SHORT_WEEKDAYS } from '@/shared/utils/weekdayIntToName'
 
@@ -15,10 +16,10 @@ export const CalendarGrid: React.FC<CalendarGridProps> = (props) => {
     const { calendarGrid } = props
 
     return (
-        <div className={styles.calendar_grid}>
+        <OverflowScroll className={styles.calendar_grid} width={900}>
             {SHORT_WEEKDAYS.map((weekdayName, idx) => {
                 return <Row key={`calendar-grid-row-${idx}`} weekdayName={weekdayName} days={calendarGrid[idx]} />
             })}
-        </div>
+        </OverflowScroll>
     )
 }
